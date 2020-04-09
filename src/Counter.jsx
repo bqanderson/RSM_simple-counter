59221930d3be198f16246380c51d5a6d2b33b3a1
Brian Q Anderson <bqanderson@me.com>
2020-04-09 17:47:53 -0500

    Persisting state with React Hook useRef.

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,38 +1,44 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 
-const useLocalStorage = (initialState, key) => {
-  const get = () => {
-    const storage = localStorage.getItem(key)
-    if (storage) return JSON.parse(storage).value
-    return initialState
-  }
+// const useLocalStorage = (initialState, key) => {
+//   const get = () => {
+//     const storage = localStorage.getItem(key)
+//     if (storage) return JSON.parse(storage).value
+//     return initialState
+//   }
 
-  const [value, setValue] = useState(get())
+//   const [value, setValue] = useState(get())
 
-  useEffect(() => {
-    localStorage.setItem(key, JSON.stringify({ value }))
-  }, [value])
+//   useEffect(() => {
+//     localStorage.setItem(key, JSON.stringify({ value }))
+//   }, [value])
 
-  return [value, setValue]
-}
+//   return [value, setValue]
+// }
 
 const Counter = ({ min, max, step }) => {
-  const [count, setCount] = useLocalStorage(0, 'count')
+  const [count, setCount] = useState(0)
+  const countRef = useRef()
 
-  const increment = () =>
-    setCount(c => {
-      if (c >= max) return c
-      return c + step
-    })
-  const decrement = () =>
-    setCount(c => {
-      if (c <= min) return c
-      return c - step
-    })
+  let message = ''
+  if (countRef.current < count) message = 'Higher'
+  if (countRef.current > count) message = 'Lower'
+
+  countRef.current = count
+
+  const increment = () => setCount(c => c + 1)
+  const decrement = () => setCount(c => c - 1)
   const reset = () => setCount(0)
+
+  useEffect(() => {
+    setTimeout(() => {
+      console.log(`Count: ${count}`)
+    }, 3000)
+  }, [count])
 
   return (
     <div className='Counter'>
+      <p>{message}</p>
       <p className='count'>{count}</p>
       <section className='controls'>
         <button onClick={increment}>Increment</button>
